0cbe3321c92315b1954991d2eff9dbe6beef3b3c
progtestandrasig <5hegyseg@gmail.com>
2020-11-13 18:55:30 +0100

    x/progtestandrasig (main) project completed - calculated is done

---
+++ week-01/day-2/MyJavaProjects/src/com/greenfox/practice/CodingHours.java
@@ -0,0 +1,26 @@
+package com.greenfox.practice;
+
+public class CodingHours {
+    public static void main(String[] args) {
+        // Egy átlagos Green Fox hallgató 6 órát kódol naponta
+        // Egy félév hosszúsága 17 hét
+        //
+        // Írd ki, hány órát tölt kódolással egy hallgató egy félév során,
+        // ha a hallgató csak hétköznap kódol
+        //
+        // Írd ki, hogy a félév hány százalékát tölti a hallgató kódolással
+        // ha az átlagos heti munkaidő 52 óra
+
+        int numberOfWeeks = 17;
+        int numberOfDaysPerWeek = 5;
+
+        int numberOfProgrammingsHoursPerDay = 6;
+        int numberOfWorkHoursPerWeek = 52;
+
+        int fullProgrammingHours = numberOfWeeks * numberOfDaysPerWeek * numberOfProgrammingsHoursPerDay;
+        int fullWorkHours = numberOfWeeks * numberOfWorkHoursPerWeek;
+
+        System.out.println("Full programmings hours = " + fullProgrammingHours);
+        System.out.println("Percent of programmings hours = " + (fullProgrammingHours / (fullWorkHours / 100.0)) + "%");
+    }
+}
