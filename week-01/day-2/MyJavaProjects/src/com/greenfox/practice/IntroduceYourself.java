7ed40fed1c037bfbab634559ae54d5f0845e18f1
progtestandrasig <5hegyseg@gmail.com>
2020-11-13 18:30:39 +0100

    IntroduceYourself project completed - three output line is ok.

---
+++ week-01/day-2/MyJavaProjects/src/com/greenfox/practice/IntroduceYourself.java
@@ -0,0 +1,19 @@
+package com.greenfox.practice;
+
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        // Írj egy programot, ami kiír rólad pár részletet a konzolra
+        // Minden egyes részletet új sorba írja a program:
+        //  - Neved
+        //  - Korod
+        //  - Magasságod méterben kifejezve (tört érték)
+        //
+        //  Példa kimenet (output):
+        //  John Doe
+        //  31
+        //  1.87
+        System.out.println("Andrási Gábor");
+        System.out.println(39);
+        System.out.println(1.81);
+    }
+}
