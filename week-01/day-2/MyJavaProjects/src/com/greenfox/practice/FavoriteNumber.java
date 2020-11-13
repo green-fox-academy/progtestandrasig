32a916700aa9e96a6178042b039b992f6fc66a5d
progtestandrasig <5hegyseg@gmail.com>
2020-11-13 19:00:57 +0100

    FavoriteNumber project completed - it's done ;)

---
+++ week-01/day-2/MyJavaProjects/src/com/greenfox/practice/FavoriteNumber.java
@@ -0,0 +1,14 @@
+package com.greenfox.practice;
+
+public class FavoriteNumber {
+    public static void main(String[] args) {
+        int favoriteNumber = 8;
+        // Írd át a fenti számot a kedvenc számoddá
+        // És utána írasd ki efféle képpen: "A kedvenc számom: 8"
+        // Kiíratáskor használd fel a favoriteNumber változót
+
+        favoriteNumber = 7;
+
+        System.out.println("A kedvenc számom: " + favoriteNumber);
+    }
+}
