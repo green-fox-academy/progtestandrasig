7d5f69da75f2d8cfb02e4dfed7253a6015a01fab
progtestandrasig <5hegyseg@gmail.com>
2020-11-13 22:43:12 +0100

    IWontCheatOnTheExams project completed - print 100 equal message

---
+++ week-01/day-2/MyJavaProjects/src/com/greenfox/practice/IWontCheatOnTheExams.java
@@ -0,0 +1,12 @@
+package com.greenfox.practice;
+
+public class IWontCheatOnTheExams {
+    public static void main(String[] args) {
+        // Írj egy programot ami 100-szor kiírja:
+        // "Nem fogok csalni a vizsgán!"
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Nem fogok csalni a vizsgán!");
+        }
+    }
+}
