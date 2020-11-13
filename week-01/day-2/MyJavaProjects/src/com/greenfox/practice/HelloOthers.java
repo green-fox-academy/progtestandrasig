d0828e0971906932a22e539b8285df8ae78c27d4
progtestandrasig <5hegyseg@gmail.com>
2020-11-13 18:25:58 +0100

    HelloOther project complete - three 'Hello' is done

---
+++ week-01/day-2/MyJavaProjects/src/com/greenfox/practice/HelloOthers.java
@@ -0,0 +1,14 @@
+package com.greenfox.practice;
+
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Üdvözöld 3 osztálytársad 3 sorban kiiratva a nevüket, ehhez hasonlóan:
+        //
+        // Helló Eszter!
+        // Helló Maris!
+        // Helló Józsi!
+        System.out.println("Helló Péter!");
+        System.out.println("Helló Katalin!");
+        System.out.println("Helló András!");
+    }
+}
