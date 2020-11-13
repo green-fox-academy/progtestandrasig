ac2ea76aac5d241a99ccbab4db3fed2668f36f15
progtestandrasig <5hegyseg@gmail.com>
2020-11-13 19:05:11 +0100

    Swap project completed - two numbers are swapd

---
+++ week-01/day-2/MyJavaProjects/src/com/greenfox/practice/Swap.java
@@ -0,0 +1,21 @@
+package com.greenfox.practice;
+
+public class Swap {
+    public static void main(String[] args) {
+        // Cseréld meg a 2 változó értékét programozás útján.
+        // Az a értéke legyen az, ami a b változóban van és fordítva.
+        int a = 123;
+
+        int b = 526;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        int c = a;
+        a = b;
+        b = c;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
