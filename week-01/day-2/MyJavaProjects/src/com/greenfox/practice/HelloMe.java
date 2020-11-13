c59dc5047b558fe70f86ee41b5d24219032ec858
progtestandrasig <5hegyseg@gmail.com>
2020-11-13 18:05:00 +0100

    HelloMe project end - a sztring kicserélve/átírva ;)

--- week-01/day-2/MyJavaProjects/src/com/greenfox/practice/HelloMe.java
+++ week-01/day-2/MyJavaProjects/src/com/greenfox/practice/HelloMe.java
@@ -3,6 +3,6 @@ package com.greenfox.practice;
 public class HelloMe {
     public static void main(String[] args) {
         // Úgy módosítsd a programot, hogy téged üdvözöljön a Világ helyett
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Gábor Andrási!");
     }
 }
