a7cb72afea1c1686d822b2599a47e6adb0bdbfa0
progtestandrasig <5hegyseg@gmail.com>
2020-11-13 22:40:39 +0100

    HelloUser project completed - read the user name and print the welcome message!

--- week-01/day-2/MyJavaProjects/src/com/greenfox/practice/HelloUser.java
+++ week-01/day-2/MyJavaProjects/src/com/greenfox/practice/HelloUser.java
@@ -1,18 +1,18 @@
 package com.greenfox.practice;
 
+import java.util.Scanner;
+
 public class HelloUser {
     public static void main(String[] args) {
         // Módosítsd úgy ezt a programot, hogy a felhasználót üdvözölje
         // a Világ helyett.
         // A program kérje a felhasználó nevét
 
-        String yourName = "";
+        Scanner scanner = new Scanner(System.in);
+
         System.out.print("What's your name?: ");
 
-        //int chars;
-        //do {
-        //    chars = (System.in.read());
-        //}while (chars != -1);
+        String yourName = scanner.next();
 
         System.out.println("Hello, " + yourName + "!");
     }
