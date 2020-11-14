63faaeaa153875573d54395454ad8eb50c06089e
progtestandrasig <5hegyseg@gmail.com>
2020-11-14 13:21:32 +0100

    ThirdElementOfArray project completed - create the array, print to console the third element of array

---
+++ week-01/day-3/MyJavaProjects/src/com/greenfox/practice/day3/ThirdElementOfArray.java
@@ -0,0 +1,15 @@
+package com.greenfox.practice.day3;
+
+import java.lang.reflect.Array;
+
+public class ThirdElementOfArray {
+    public static void main(String[] args) {
+        // - Készíts egy `numbers` nevű változót (tömböt)
+        //   a következő tartalommal (értékekkel): `[4, 5, 6, 7]`
+        // - Írasd ki a konzolra a `numbers` nevű tömb harmadik elemét
+
+        int[] numbers = {4, 5, 6, 7};
+
+        System.out.println("3. elem: " + numbers[2]);
+    }
+}
