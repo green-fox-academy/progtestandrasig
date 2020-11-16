48dffb598fbcbe8c289b258e28eca7d07ad3784d
progtestandrasig <5hegyseg@gmail.com>
2020-11-16 13:23:16 +0100

    StudentCounter project completed - had counted the students from the Map

---
+++ week-01/day-4/MyJavaProjects/src/com/greenfox/practice/StudentCounter.java
@@ -0,0 +1,67 @@
+package com.greenfox.practice.day4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StudentCounter {
+    public static void main(String... args) {
+        List<Map<String, Object>> listOfMaps = new ArrayList<>();
+
+        // Adott az alábbi HashMap-ek listája:
+
+        Map<String, Object> row0 = new HashMap<>();
+        row0.put("name", "Theodor");
+        row0.put("age", 9.5);
+        row0.put("candies", 2);
+        listOfMaps.add(row0);
+
+        Map<String, Object> row1 = new HashMap<>();
+        row1.put("name", "Paul");
+        row1.put("age", 10);
+        row1.put("candies", 1);
+        listOfMaps.add(row1);
+
+        Map<String, Object> row2 = new HashMap<>();
+        row2.put("name", "Mark");
+        row2.put("age", 7);
+        row2.put("candies", 3);
+        listOfMaps.add(row2);
+
+        Map<String, Object> row3 = new HashMap<>();
+        row3.put("name", "Peter");
+        row3.put("age", 12);
+        row3.put("candies", 5);
+        listOfMaps.add(row3);
+
+        Map<String, Object> row4 = new HashMap<>();
+        row4.put("name", "Olaf");
+        row4.put("age", 12);
+        row4.put("candies", 7);
+        listOfMaps.add(row4);
+
+        Map<String, Object> row5 = new HashMap<>();
+        row5.put("name", "George");
+        row5.put("age", 3);
+        row5.put("candies", 2);
+        listOfMaps.add(row5);
+
+        // Írasd ki az alábbi dolgokat:
+        //  - Azokat a diákokat akiknek több mint 4 édességük van
+        //  - Azon diákok korának az összegét, akiknek kevesebb mint 5 édességük van
+
+        List<String> minFourCandies = new ArrayList<>();
+        double sumOfAgesHasLoserStudents = 0;
+
+        for (Map row : listOfMaps) {
+
+            if ((int)row.get("candies") > 4)
+                minFourCandies.add(row.get("name").toString());
+            else sumOfAgesHasLoserStudents += Double.parseDouble(row.get("age").toString());
+        }
+
+        System.out.println("Min. four candies..: " + String.join(", ", minFourCandies));
+        System.out.println("Sum of ages has loser students..: " + sumOfAgesHasLoserStudents);
+    }
+}
