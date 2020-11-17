81b947d1809aa0eab44340476a06ecc79304d862
progtestandrasig <5hegyseg@gmail.com>
2020-11-17 23:06:18 +0100

    PalindromeSearcher project completed - searchPalindrome funcion is return the palindromes's list, but not full ready...? :(

---
+++ week-01/day-5/MyJavaProjects/src/com/greenfox/practice/PalindromeSearcher.java
@@ -0,0 +1,81 @@
+package com.greenfox.practice.day5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PalindromeSearcher {
+    public static void main(String[] args) {
+
+//        Palindróma kereső
+//        Mi is az a palindróma?
+//
+//        Gyakorlat
+//        Készíts egy searchPalindrome nevű függvényt, ami követi a jelenlegi nyelv stílus útmutatóját. Bemeneten fogad egy stringet. Majd kikeresi, hogy taláhatóak-e 3 karakternél hoszabb palindrómák benne és végül visszaküldi a talált palindrómák listáját.
+//
+//        Példák:
+//        bemenet	                            kimenet
+//        "dog goat dad duck doodle never"	    ["og go", "g g", " dad ", "dad", "d d", "dood", "eve"]
+//        "apple"	                            []
+//        "racecar"	                            ["racecar", "aceca", "cec"]
+//        ""	                                []
+
+        String text = "dog goat dad duck doodle never";
+
+        System.out.println(searchPalindrome(text));
+    }
+
+    private static List<String> searchPalindrome(String text) {
+
+        //if (text.length() % 2 == 1)
+        //    return new ArrayList<>();
+
+        if (text.length() < 3)
+            return new ArrayList<>();
+
+        List<String> subPalindromes = new ArrayList<>();
+
+        int length = 3;
+        int maxLength = text.length() - 3;
+
+        String subString;
+        boolean hasASubPalindrome = false;
+
+        for (int i = length; i < text.length() - length; i++) {
+
+            for (int j = 0; j <= text.length() - length && !hasASubPalindrome; j++) {
+
+                subString = text.substring(j, j + length);
+
+                if (isASubPalindrome(subString)) {
+                    subPalindromes.add(subString);
+                    hasASubPalindrome = true;
+                }
+
+                if (j == text.length() - length) {
+                    length++;
+                    hasASubPalindrome = false;
+                }
+                else if (j < text.length() - length && hasASubPalindrome) {
+
+                    hasASubPalindrome = false;
+                }
+            }
+
+            hasASubPalindrome = false;
+        }
+
+        return subPalindromes;
+    }
+
+    private static boolean isASubPalindrome(String subString) {
+
+        boolean isAPalindrome = true;
+
+        for (int i = 0; i < subString.length() / 2 && isAPalindrome; i++) {
+
+            if (subString.charAt(i) != subString.charAt(subString.length() - 1 - i)) isAPalindrome = false;
+        }
+
+        return  isAPalindrome;
+    }
+}
