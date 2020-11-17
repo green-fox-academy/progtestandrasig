1e5c2081ef1c27d1642885470fa74500a763b419
progtestandrasig <5hegyseg@gmail.com>
2020-11-17 13:13:36 +0100

    project completed - appendAFunc function is works

---
+++ week-01/day-5/MyJavaProjects/src/com/greenfox/practice/Sum.java
@@ -0,0 +1,25 @@
+package com.greenfox.practice.day5;
+
+public class Sum {
+    public static void main(String[] args) {
+
+// - Hozz létre egy class-t `Sum` névvel és benne a main függvényt.
+
+// - Készíts egy függvényt `sum` névvel mely összeadja a számokat
+//   nullától a bemeneti paraméterig és visszaadja azok összegét.
+
+        int maxNumber = 100;
+
+        System.out.println("Sum of 0.." + maxNumber + " is equal " + sum(maxNumber));
+    }
+
+    private static int sum(int maxNumber) {
+
+        int summa = 0;
+        for (int i = 1; i <= maxNumber; i++) {
+            summa += i;
+        }
+
+        return summa;
+    }
+}
